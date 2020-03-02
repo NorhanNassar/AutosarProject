5ee0d863534e2520e1d924ad0b0e1283080f3901
mostafamoh1236 <mostafamoh1236@gmail.com>
2020-03-02 18:37:54 +0200

    Editing the APP layer components (Left / right Door) and the Lighting components and adding the comments to these files Signed-off-by: mostafamoh1236 <mostafamoh1236@gmail.com>

--- 03_APP/01_LeftDoor/Left_Door.c
+++ 03_APP/01_LeftDoor/Left_Door.c
@@ -1,18 +1,47 @@
+/************************************************************************
+ * File name: Left_Door.c
+ *
+ *
+ * Description: This file contains the implementation of the APIs of the 
+ * Left Door Component
+ *
+ * owner: Mostafa Ramadan
+ * date: 2/3/2020
+ * version 1.1
+ *
+ ***************************************************************************/
+
+/* The includes needed to initialize the left door component */
 #include "STD_TYPES.h"
 #include "DoorSensor.h"
 #include "Left_Door.h"
 
-
+/* this function is to initialize the left door and map it to the corresponding 
+ * Door Sensor and return the error status of the function if it failed or succeeded */
 Error_Status leftDoor_Init(void)
 {
+  /* Initialize the local error as E_OK status */
   Error_Status Local_Error = E_OK;
+  
+  /* Mapping the left door to the corresponding LEFT_DOOR Sensor and return 
+   * the error status of the function if it is failed or succeeded */
   Local_Error = DoorSensor_Init(LEFT_DOOR);
+  
+  /* Return the status of the function E_NOK if there is a fail or E_OK if succeeded */
   return Local_Error;
 }
 
+/* this function is to read the status of the left door and check if it is opened or not 
+ * and return the error status of the function if it failed or succeeded */
 Error_Status LeftDoor_ReadStatus(u8 *Door_Status)
 {
+  /* Initialize the local error as E_OK status */
   Error_Status Local_Error = E_OK;
+  
+  /* reading the status of the left door if it is opened or not and return
+   * the error status of the function if it failed or succeeded */
   Local_Error = DoorSensor_ReadStatus(LEFT_DOOR,Door_Status);
+  
+  /* Return the status of the function E_NOK if there is a fail or E_OK if succeeded */
   return Local_Error;
 }
--- 03_APP/02_RightDoor/Right_Door.h
+++ 03_APP/02_RightDoor/Right_Door.h
@@ -1,11 +1,46 @@
+/************************************************************************
+ * File name: Right_Door.h
+ *
+ *
+ * Description: This file contains the prototypes of the Right Door driver
+ * and the macros used by the user
+ *
+ * owner: Mostafa Ramadan
+ * date: 2/3/2020
+ * version 1.1
+ *
+ ***************************************************************************/
 #ifndef _RIGHT_DOOR_H__
 #define _RIGHT_DOOR_H__
 
-/* Configurations for Left Door sensors */
+/* Configuration for Right Door sensor mapping */
 #define RIGHT_DOOR  DOOR_SENSOR_CH0
-/***************************************/
 
+
+/************************************************************************
+ * Function name: RightDoor_Init
+ *
+ * parameters:  Input: NA
+ *              Output: NA
+ *              In/out: NA
+ * return: E_OK, E_NOK
+ * Description: a function to intialize the right door and map it to the corresponding 
+ *              door sensor
+ ***************************************************************************/
 Error_Status RightDoor_Init(void);
+
+
+/************************************************************************
+ * Function name: RightDoor_ReadStatus
+ *
+ * parameters:  Input: NA
+ *              Output: NA
+ *              In/out: Door_Status
+ *                     type: pointer to u8
+ *                      Description: Read if the door is opened or not opened
+ * return: E_OK, E_NOK
+ * Description: a function to read if the right door is opened or not 
+ ***************************************************************************/
 Error_Status RightDoor_ReadStatus(u8 * Door_Status);
 
 #endif
